04b9a7cb1ec8e91d2006e65e8df9312d97a3af03
Wesley Bernardes <bw3sley@gmail.com>
2024-03-01 22:07:52 -0300

    :tada: chore: solve big countries challenge

---
+++ study-plans/sql-50/select/big-countries/solution.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+SELECT 
+    World.name, 
+    World.population, 
+    World.area 
+FROM World 
+WHERE World.population >= 25000000 
+    OR World.area >= 3000000
